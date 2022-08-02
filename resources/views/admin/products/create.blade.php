6c303ecd1e0a17b150eabe87330bd0ae57f6e6d6
mannguyen <nguyentrongminhman95@gmail.com>
2022-08-02 23:14:35 +0700

    update tinymce

--- resources/views/admin/products/create.blade.php
+++ resources/views/admin/products/create.blade.php
@@ -7,7 +7,7 @@
 @endsection
 
 @section('css')
-
+<link href="{{ asset('backend/dist/css/select2.min.css')}}" rel="stylesheet" />
 @endsection
 
 
@@ -30,42 +30,40 @@
                     </div>
 
                     <div class="form-group">
-                        <label>Content</label>
-                        <textarea class="form-control" name="content" rows="3" placeholder="Enter ..."></textarea>
-                    </div>
-
-
-                    <div class="form-group">
-                        <select class="form-control select2" name="parent_id" style="width: 100%;">
-                          <option selected="selected" value="0">Choose Category Product</option>
+                        <select class="form-control select-category" name="parent_id" style="width: 100%;">
+                          <option selected="selected" value="">Choose Category Product</option>
                           {!! $htmlOption !!}
                         </select>
                     </div>
 
+
+                    <div class="form-group">
+                      <label>Content</label>
+                      <textarea class="form-control tinymce_editor_init" name="content" rows="15"></textarea>
+                    </div>
+
+                    <div class="form-group">
+                      <label>Choose Tags</label>
+                      <select class="form-control choose-tags" name="tags[]" multiple="multiple">
+                        <option selected="selected">orange</option>
+                        <option>white</option>
+                        <option selected="selected">purple</option>
+                      </select>
+                    </div>
+
+
                     <div class="form-group choose-input">
                         <label for="exampleInputFile">Feature Image</label>
-                        <div class="input-group">
-                          <div class="custom-file">
-                            <input type="file" class="custom-file-input" id="exampleInputFile" name="feature_image_path">
-                            <label class="custom-file-label" for="exampleInputFile">Choose file</label>
-                          </div>
-                        </div>
+                        <input type="file" class="form-control-file" id="exampleInputFile" name="feature_image_path">
                     </div>
-
 
                     <div class="form-group choose-input">
-                        <label for="exampleInputFile">List Image</label>
-                        <div class="input-group">
-                          <div class="custom-file">
-                            <input type="file" multiple class="custom-file-input" id="exampleInputFile" name="image_path[]">
-                            <label class="custom-file-label" for="exampleInputFile">Choose file</label>
-                          </div>
-                        </div>
+                        <label>List Image</label>
+                        <input type="file" multiple class="form-control-file" id="exampleInputFile" name="image_path[]">
                     </div>
 
-                   
-
-
+                    
+                    
                     <button class="btn btn-primary">Create Product</button>
                 </form>
             </div>
@@ -76,12 +74,9 @@
 @endsection
 
 @section('js')
-    <script>
-        $(".custom-file-input").on("change", function() {
-            var fileName = $(this).val().split("\\").pop();
-            $(this).siblings(".custom-file-label").addClass("selected").html(fileName);
-        });
-    </script>
+  <script src="{{ asset('backend/dist/js/select2.min.js')}}"></script>
+  <script src="{{ asset('backend/admin/product/create/tinymce.min.js')}}"></script>
+  <script src="{{ asset('backend/admin/product/create/create.js')}}"></script>
 @endsection
 
 
