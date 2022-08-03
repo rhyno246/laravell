9dae8c8bfb9c86c67969548326949f0853964a11
jimmy.nguyen <jimmy.nguyen@nexcel.vn>
2022-08-03 15:22:25 +0700

    update

--- resources/views/admin/products/create.blade.php
+++ resources/views/admin/products/create.blade.php
@@ -75,7 +75,7 @@
 
 @section('js')
   <script src="{{ asset('backend/dist/js/select2.min.js')}}"></script>
-  <script src="{{ asset('backend/admin/product/create/tinymce.min.js')}}"></script>
+  <script src="https://cdn.tiny.cloud/1/g206zibscsg4symvdigxh4b8rpbbp5oc6rj1r8ceb7p3jncb/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
   <script src="{{ asset('backend/admin/product/create/create.js')}}"></script>
 @endsection
 
